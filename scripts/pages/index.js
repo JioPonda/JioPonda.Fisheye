e227f356042861496e330b90af2c9fbd036cc1c5
Jioponda <jioponda@outlook.fr>
2022-08-08 14:49:04 +0200

    ajout photo de profil et changement tableau

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -12,50 +12,50 @@
                 "country": "UK",
                 "tagline": "Voir le beau dans le quotidien",
                 "price": 400,
-                "portrait": "MimiKeel.jpg"
+                "portrait": "./mimi/Portrait_Nora.jpg"
             },
             {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },{
-                "name": "Ma data test",
-                "id": 1,
+                "name": "Ellie-Rose Wilkens",
+                "id": 930,
                 "city": "Paris",
                 "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
+                "tagline": "Capturer des compositions complexes",
+                "price": 250,
+                "portrait": "./Ellie_Rose/Architecture_Horseshoe.jpg"
             },{
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
+                "name": "Tracy Galindo",
+			    "id": 82,
+			    "city": "Montreal",
+			    "country": "Canada",
+			    "tagline": "Photographe freelance",
+			    "price": 500,
+			    "portrait": "./Tracy/Fashion_Urban_Jungle.jpg"
             },
             {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
+                "name": "Nabeel Bradford",
+                "id": 527,
+                "city": "Mexico City",
+                "country": "Mexico",
+                "tagline": "Toujours aller de l'avant",
+                "price": 350,
+                "portrait": "./Nabeel/Travel_Outdoor_Baths.jpg"
+            },{
+                "name": "Rhode Dubois",
+                "id": 925,
+                "city": "Barcelona",
+                "country": "Spain",
+                "tagline": "Je crée des souvenirs",
+                "price": 275,
+                "portrait": "./Rhode/Fashion_Melody_Red_on_Stripes.jpg"
+            },
+            {
+                "name": "Marcel Nikolic",
+                "id": 195,
+                "city": "Berlin",
+                "country": "Germany",
+                "tagline": "Toujours à la recherche de LA photo",
+                "price": 300,
+                "portrait": "./Marcel/Travel_Tower.jpg"
             },
         ]
         // et bien retourner le tableau photographers seulement une fois
