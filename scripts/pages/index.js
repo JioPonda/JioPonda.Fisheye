bc80da765f55c546f44f7d1816dbd466fffbd566
Jioponda <jioponda@outlook.fr>
2022-09-24 11:19:50 +0200

    modification avant présentation

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,32 +1,32 @@
-    async function getPhotographers() {
+async function getPhotographers() {
 
-        await fetch(
-            "./data/photographers.json"
-          ) /** Attendre la récupération des données JSON */
-          .then((res) => res.json()) 
-          /** Alors ce résultat est transformé en DATA (objet javascript) */
-          .then((data) => (photographers = data.photographers));
-          /** Récupération dans DATA des données photographers */
-          return {photographers: [...photographers,] 
-          /** On retourne un tableau avec les données des photographes */,
-          };
-    }
-    
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+    await fetch(
+        "./data/photographers.json"
+      ) /** Attendre la récupération des données JSON */
+      .then((res) => res.json()) 
+      /** Alors ce résultat est transformé en DATA (objet javascript) */
+      .then((data) => (photographers = data.photographers));
+      /** Récupération dans DATA des données photographers */
+      return {photographers: [...photographers,] 
+      /** On retourne un tableau avec les données des photographes */,
+      };
+}
 
-        photographers.forEach((photographer) => {
-            console.log(photographer);
-            const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    };
+async function displayData(photographers) {
+    const photographersSection = document.querySelector(".photographer_section");
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    };
-    
-    init();
+    photographers.forEach((photographer) => {
+        console.log(photographer);
+        const photographerModel = photographerFactory(photographer);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersSection.appendChild(userCardDOM);
+    });
+}
+
+async function init() {
+    // Récupère les datas des photographes
+    const { photographers } = await getPhotographers();
+    displayData(photographers);
+}
+
+init();
